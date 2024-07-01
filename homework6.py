c5b5a723d7bfc40a03d58a2c1a9be39caed42aaf
EldarGafizov <eldar.gafizov@mail.ru>
2024-07-02 00:23:02 +0300

    Update homework6.py

--- homework6.py
+++ homework6.py
@@ -1,10 +1,11 @@
 my_dict = {'Vasya': 1975, 'Egor': 1999, 'Masha': 2002}
 print(my_dict)
 print(my_dict['Masha'])
+print(my_dict.get('Kamila'))
 my_dict['Kamila'] = 1981
 my_dict['Artem'] = 1915
 print(my_dict)
-del my_dict['Egor']
+print(my_dict.pop('Egor'))
 print(my_dict)
 
 my_set = {1, 2, 3, 4, 5, 1, 2, 3, 4, 'Яблоко', 42.314}
